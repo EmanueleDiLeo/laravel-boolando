86fa172c1d5d945831e2f3524d7bd3d0981b4359
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-11-17 21:09:03 +0100

    [ADD] : products

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+
+@section('content')
+
+    <h1>Benvenuto in chi siamo</h1>
+
+@endsection
+
+
+@section('title')
+    | Chi Siamo
+@endsection
---
+++ resources/views/products.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+
+
+@section('content')
+
+    <div class="container flex">
+
+        @foreach ($products as $item)
+        <div class="card">
+            <div class="cont-img">
+              <img src="/img/{{$item['img']}}" title="{{$item['brand']}}">
+              <img class="switch" src="/img/{{$item['imgHover']}}" alt="{{$item['brand']}}" title="{{$item['brand']}}">
+            </div>
+
+            <div class="box-position">
+                @if ($item['discount'] != null)
+                    <span class="offer">{{$item['discount']}}</span>
+                @endif
+
+                @if ($item['green'] != null)
+                    <span class="green">{{$item['green']}}</span>
+                @endif
+            </div>
+
+            <span class="heart {{$item['favorite'] === true ? 'active' : ''}}">&hearts;</span>
+
+            <span>{{$item['brand']}}</span>
+            <h2>{{$item['name']}}</h2>
+            <span class="price">{{$item['price']}} </span>
+            @if ($item['priceOld'] != null)
+                <span class="old-price">{{$item['priceOld']}}</span>
+            @endif
+
+          </div>
+        @endforeach
+
+    </div>
+
+@endsection
+
+
+@section('title')
+    | Prodotti
+@endsection
