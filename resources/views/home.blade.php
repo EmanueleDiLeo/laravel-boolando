3401b9a0efd0d7e5b647d5233ac5ab1b1fe4e37d
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-11-17 18:32:26 +0100

    [ADD] : Header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+
+@section('content')
+
+
+@endsection
+
+
+@section('title')
+    | Homepage
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+<?php
+    $filters = config('menues.filters');
+    $profile = config('menues.profile');
+?>
+
+<header>
+    <div class="container flex">
+        <nav class="main-menu">
+            <ul class="flex">
+                @foreach ($filters as $item)
+                    <li>
+                        <a href="{{$item['href']}}"><strong>{{$item['text']}}</strong></a>
+                    </li>
+                @endforeach
+            </ul>
+        </nav>
+        <img src="/img/boolean-logo.png" alt="logo" title="logo">
+        <nav class="shop-menu">
+            <ul class="flex">
+                @foreach ( $profile as $item )
+                    <li>
+                        <a href="{{$item['href']}}"><i class="{{$item['icon']}}"></i></a>
+                    </li>
+                @endforeach
+
+            </ul>
+        </nav>
+    </div>
+</header>
